55ca99f0591ad9ee2bd613d1487a9387cf98f5ad
Steph Locke <slocke@mango-solutions.com>
2016-01-31 21:50:48 +0000

    Better spacing

--- reportGen.R
+++ reportGen.R
@@ -2,13 +2,16 @@ reportGenUI <- function(id) {
   ns<-NS(id)
   tagList(actionButton(ns("make"),"Generate file")
           ,downloadButton(ns("download"),"Download file"))
-  }
+}
 
 reportGen <- function(input,output,session, period) {
-tmp<-tempfile(fileext = ".pdf")
+  tmp<-tempfile(fileext = ".pdf")
+  
   fragRender<-function(type, period){
     content<-callModule(keyData,type,type,period)
+    
     title <- sections$section[sections$alias == type]
+    
     readr::read_file(
       rmarkdown::render(
         input = "boardfragment.Rmd"
@@ -21,7 +24,6 @@ tmp<-tempfile(fileext = ".pdf")
           ,type = type
           ,title = title
         )))
-   # paste("#",type,"\n",period, sep=" ")
   }
   
   observeEvent(input$make,{
@@ -30,15 +32,15 @@ tmp<-tempfile(fileext = ".pdf")
     write(toAdd,"dummy.Rmd", append=TRUE)
     rmarkdown::render("dummy.Rmd",output_file=tmp)
   } )
- 
-
+  
+  
   output$generate <- downloadHandler(
     filename = myOutputFileName
     ,content = function(file) {
       rmarkdown::render("dummy.Rmd")
       file.rename("dummy.pdf",file)
-      }
+    }
     ,contentType = "application/pdf"
-)
+  )
   
 }
