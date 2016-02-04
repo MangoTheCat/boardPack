d9e252a04e1fc8e3df7f7032c80792f6ed7b7d17
Steph Locke <slocke@mango-solutions.com>
2016-02-04 14:34:27 +0000

    Corrected download capability (#2)

--- reportGen.R
+++ reportGen.R
@@ -1,42 +1,35 @@
 reportGenUI <- function(id) {
   ns<-NS(id)
-  tagList(actionButton(ns("make"),"Generate file")
-          ,downloadButton(ns("download"),"Download file"))
+  tagList(downloadButton(ns("generate")
+                         ,"Download file"))
 }
 
 reportGen <- function(input,output,session, period) {
   tmp<-tempfile(fileext = ".pdf")
   
-  fragRender<-function(type, period){
-    content<-callModule(keyData,type,type,period)
+   fragRender<-function(type, period){
+    contents<-callModule(keyData,id=type,type,period)
     
     title <- sections$section[sections$alias == type]
     
     readr::read_file(
-      rmarkdown::render(
-        input = "boardfragment.Rmd"
-        ,output_format = rmarkdown::md_document()
-        ,output_file=tempfile(fileext = ".md")
-        ,output_dir = tempdir()
-        ,params = list(
-          comment = input$commentary
-          ,period = period
-          ,type = type
-          ,title = title
-        )))
-  }
-  
-  observeEvent(input$make,{
-    file.copy("boardpack.Rmd", "dummy.Rmd", overwrite = TRUE)
-    toAdd<-paste("\n",lapply(sections[,alias], fragRender, period=period))
-    write(toAdd,"dummy.Rmd", append=TRUE)
-    rmarkdown::render("dummy.Rmd",output_file=tmp)
-  } )
-  
+      customMDRender(type
+                     ,list(comment = input$commentary
+                           ,period = period
+                           ,type = type
+                           ,title = title
+                     )
+                     ,rmarkdown::md_document()
+      )
+    )
+    }
   
   output$generate <- downloadHandler(
     filename = myOutputFileName
     ,content = function(file) {
+      file.copy("boardpack.Rmd", "dummy.Rmd", overwrite = TRUE)
+      toAdd<-paste("\n",lapply(sections[,alias], fragRender, period=period))
+      write(toAdd,"dummy.Rmd", append=TRUE)
       rmarkdown::render("dummy.Rmd")
       file.rename("dummy.pdf",file)
     }
